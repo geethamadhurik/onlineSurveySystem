47d5b37e11e7e0d0fb0cd938412f72510ff676ba
geethamadhurik <139434985+geethamadhurik@users.noreply.github.com>
2023-07-30 14:29:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package org.jsp.surveyApp;
+import java.sql.SQLException;
+public class Main {
+	public static void main(String args[]) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
